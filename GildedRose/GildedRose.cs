d9ee0b99e0a561f28d000a04eb931fd0e2e822b3
Nicolas <Nicolas@PC-NICOLAS>
2022-11-15 16:30:46 +0100

    - Add unit tests to legacy code for each business rule - Refactor the UpdateQuality method by :   - removing all static item names   - strongly typing the different items   - moving specific business logic inside the business object

---
+++ GildedRose/GildedRose.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+
+namespace GildedRoseKata
+{
+    public class GildedRose
+    {
+        private readonly IList<Item> Items;
+        public GildedRose(IList<Item> Items)
+        {
+            this.Items = Items;
+        }
+
+        public void UpdateQuality()
+        {
+            for (var i = 0; i < Items.Count; i++)
+            {
+                switch (Items[i])
+                {
+                    case LegendaryItem l: break;
+                    case AgingItem a:
+                        a.SetAgingItemQuality();
+                        break;
+                    case ConjuredItem c:
+                        DecreaseItemProperties(c);
+                        DecreaseItemQuality(c);
+                        break;
+                    default:
+                        DecreaseItemProperties(Items[i]);
+                        break;
+                }
+            }
+        }
+
+        private void DecreaseItemProperties(Item item)
+        {
+            DecreaseItemQuality(item);
+            DecreateItemSellIn(item);
+            DecreaseItemQuality(item);
+        }
+
+        private void DecreaseItemQuality(Item item)
+        {
+            if (item.Quality > 0)
+            {
+                item.Quality--;
+            }
+        }
+
+        private void DecreateItemSellIn(Item item)
+        {
+            if (item is not LegendaryItem)
+            {
+                item.SellIn--;
+            }
+        }
+        
+    }
+}
